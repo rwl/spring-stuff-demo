f9a05f2c005a80d950e692d50bb213c2a3203288
rwl <r.w.lincoln@gmail.com>
2012-04-15 02:52:00 +0100

    Initial commit.

---
+++ src/main/java/com/example/MyApplicationBean.java
@@ -0,0 +1,12 @@
+
+package com.example;
+
+import org.dellroad.stuff.vaadin.SpringContextApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Example of a Spring bean that lives in the per-Applicaton application context.
+ */
+public class MyApplicationBean extends AbstractApplicationBean {
+}
+
